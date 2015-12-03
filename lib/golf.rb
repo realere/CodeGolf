8a0668491f23113dc05a2a214a62ce6a25bc7591
kieranpringle <contact@kieranpringle.com>
2015-12-03 11:23:16 +0000

    yay

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,8 +1,32 @@
 class Golf
   
-	def hole1(num)
+	def self.hole1(num)
 		arr = []
-		1.upto(num)
+		
+		1.upto(num) do |i|
+			ppp = false
+			arr[i-1] = "" 
+			if i%3<1
+				arr[i-1]+="pling"
+				ppp = true
+			end
+			if i%5<1
+				arr[i-1]+="plang"
+				ppp = true
+			end
+			if i%7<1
+				arr[i-1]+="plong"
+				ppp = true
+			end
+			unless ppp
+				arr[i-1] = i
+			end
+		end
+		arr
+	end
+
+	def self.hole2(string)
+		string.split(" ").map{|w| w[0]}.join("")
 	end
 
 end
